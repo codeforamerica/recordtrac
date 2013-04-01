16dc066e7f2d02c4ce0cf5d454261d9cceffb9e9
Richa Agarwal <richa.agarwal85@gmail.com>
2013-04-01 16:53:08 -0700

    Adding error message if duplicate request

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, flash
 from upload import upload
 from flask.ext.sqlalchemy import SQLAlchemy
+# from flask.ext.sqlalchemy.exceptions import *
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/mydatabase'
 db = SQLAlchemy(app)
@@ -38,6 +39,22 @@ def show_request(request_id):
     			doc_ids.append("Nothing uploaded yet by %s" % owner.name)
     return render_template('requested.html', text = r.text, request_id = request_id, doc_ids = doc_ids, status = r.status)
 
+
+
+# @app.route('/request/edit/<int:request_id>', methods=['GET', 'POST'])
+# def edit_request(request_id):
+# 	if request.method == 'POST':
+# 		owner_id = request.form['owner_id']
+# 		db.session.delete(Owner.query.get(owner_id))
+# 	# edit the request with the given id
+# 	owner_ids = []
+# 	owner_emails = []
+# 	req = Request.query.get(request_id)
+# 	if req.owners:
+# 		for owner in req.owners:
+# 			owner_ids.append(owner.id)
+# 			owner_emails.append(owner.email)
+
 def assign_owner(request_id, name, email):
 	contact = Contact(email)
 	db.session.add(contact)
@@ -59,13 +76,19 @@ def unassign_owner(owner_id):
 # 	print "Owner"
 
 def make_request(str):
-	req = Request(str)
-	db.session.add(req)
-	db.session.commit()
-	owner_id = assign_owner(req.id, "richa", "richa@codeforamerica.org")
-	# unassign_owner(owner_id)
-	open_request(req.id)
-	return req.id
+	try:
+		req = Request(str)
+		db.session.add(req)
+		db.session.commit()
+		owner_id = assign_owner(req.id, "richa", "richa@codeforamerica.org")
+		# unassign_owner(owner_id)
+		open_request(req.id)
+		return req.id
+	except:
+		return None
+	# except exc.InvalidRequestError:
+	# 	return None
+
 
 
 def change_request_status(id, status):
@@ -99,11 +122,14 @@ def close_request(id):
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'POST':
-		text = request.form['request_text']
-		request_id = make_request(text)
-		return show_request(request_id)
-	else:
-		return render_template('index.html')
+		request_text = request.form['request_text']
+		request_id = make_request(request_text)
+		if request_id:
+			return show_request(request_id)
+		else:
+			req = Request.query.filter_by(text = request_text).first()
+			return render_template('error.html', message = "Your request is the same as /request/%s" % req.id)
+	return render_template('index.html')
 
 @app.route('/requests', methods=['GET', 'POST'])
 def requests():
