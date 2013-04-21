16715025f4c499766648c4aa6ba87182ae690f4b
criscristina <cris@codeforamerica.org>
2013-04-21 13:24:34 -0700

    fixed navbar active page indicator

--- static/main.js
+++ static/main.js
@@ -1,1 +1,31 @@
+/* banner dismissal */
 $(function(){$(".alert").alert()})
+
+// $(document).ready(function () {
+// 	$('.nav li').click(function() {
+// 		$('.nav li:active').removeClass('active');
+// 	 $('this').addClass('active');
+// 	});
+// });
+
+
+/* navbar active page indicator */
+$(function(){
+  function stripTrailingSlash(str) {
+    if(str.substr(-1) == '/') {
+      return str.substr(0, str.length - 1);
+    }
+    return str;
+  }
+
+  var url = window.location.pathname;  
+  var activePage = stripTrailingSlash(url);
+
+  $('.nav li a').each(function(){  
+    var currentPage = stripTrailingSlash($(this).attr('href'));
+
+    if (activePage == currentPage) {
+      $(this).parent().addClass('active'); 
+    } 
+  });
+});
