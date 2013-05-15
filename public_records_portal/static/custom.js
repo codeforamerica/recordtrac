626e861985c268cf1aa08371c7be2c80a407ffcc
criscristina <cris@codeforamerica.org>
2013-05-15 16:56:27 -0700

    history popover content and close dropdown

--- public_records_portal/static/custom.js
+++ public_records_portal/static/custom.js
@@ -50,7 +50,8 @@
   });
 
   /* form in history popover */
-  $('#historyPopover').popover({ 
+  $('#historyPopover').popover({
+      trigger: 'hover', 
       html : true,
       title: function() {
         return $("#historyPopover-head").html();
