0dd66e67a2a808a5018363da4a38060568fa9338
Richa Agarwal <richa.agarwal85@gmail.com>
2013-05-21 15:07:33 -0700

    Updating config

--- public_records_portal/__init__.py
+++ public_records_portal/__init__.py
@@ -1,13 +1,12 @@
-# import os 
+from os import environ
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
 
 # Initialize Flask app and database:
 app = Flask(__name__)
+app.config.from_object('websiteconfig')
 db = SQLAlchemy(app)
 db.create_all()
-
-app.config.from_object('websiteconfig')
-heroku = Heroku(app) 
+ 
 from public_records_portal import prflask
