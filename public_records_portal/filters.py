9c68f63a08c1caf65a44a59171ba620fc9da637f
Richa Agarwal <richa.agarwal85@gmail.com>
2013-05-21 09:48:37 -0700

    Using API calls for most things now

--- public_records_portal/filters.py
+++ public_records_portal/filters.py
@@ -6,6 +6,7 @@ import os
 # Get filepath for actions.json
 actions_filepath = os.path.join(app.root_path, 'actions.json')
 
+
 # Filters
 
 @app.template_filter('date')
@@ -45,26 +46,27 @@ def date_granular(timestamp):
 
 @app.template_filter('owner_name')
 def owner_name(oid):
-	owner = get_resource("owner", app.config['APPLICATION_URL'], oid)
-	user = get_resource("user", app.config['APPLICATION_URL'], owner['user_id'])
+	owner = get_resource("owner", oid)
+	user = get_resource("user", owner['user_id'])
 	if user['alias']:
 		return user['alias']
 	return user['email']
 
 @app.template_filter('subscriber_name')
 def subscriber_name(sid):
-	subscriber = get_resource("subscriber", app.config['APPLICATION_URL'], sid)
+	subscriber = get_resource("subscriber", sid)
 	return user_name(subscriber['user_id'])
 
 @app.template_filter('user_name')
 def user_name(uid):
-	user = get_resource("user", app.config['APPLICATION_URL'], uid)
+	user = get_resource("user", uid)
 	if user['alias']:
 		return user['alias']
 	return user['email']
 
 @app.template_filter('explain_action')
 def explain_action(action):
-	action_json = open(actions_filepath)
-	json_data = json.load(action_json)
-	return json_data[action]
+	return action
+	# action_json = open(actions_filepath)
+	# json_data = json.load(action_json)
+	# return json_data[action]
