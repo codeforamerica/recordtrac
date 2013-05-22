0e6e7810ce3916ec9bfe764f5a483154824c96ca
Richa Agarwal <richa.agarwal85@gmail.com>
2013-05-22 15:35:06 -0700

    Heroku config

--- public_records_portal/__init__.py
+++ public_records_portal/__init__.py
@@ -1,14 +1,25 @@
 from os import environ
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.heroku import Heroku
 
 # Initialize Flask app and database:
 app = Flask(__name__)
 try:
 	app.config.from_object('local_config')
 except:
-	heroku = Heroku(app) # Sets app.config from Heroku variables, that should be set already.
+	app.config['ENVIRONMENT'] = environ['ENVIRONMENT']
+	app.config['APPLICATION_URL'] = environ['APPLICATION_URL']
+	app.config['SCRIBD_API_KEY'] = environ['SCRIBD_API_KEY']
+	app.config['SCRIPT_API_SECRET'] = environ['SCRIBD_API_SECRET']
+	app.config['DEFAULT_OWNER_NAME'] = environ['DEFAULT_OWNER_NAME']
+	app.config['DEFAULT_OWNER_EMAIL'] = environ['DEFAULT_OWNER_EMAIL']
+	app.config['DEFAULT_OWNER_REASON'] = environ['DEFAULT_OWNER_REASON']
+	app.config['MAIL_USERNAME'] = environ['MAIL_USERNAME']
+	app.config['MAIL_PASSWORD'] = environ['MAIL_PASSWORD']
+	app.config['DEFAULT_MAIL_SENDER'] = environ['DEFAULT_MAIL_SENDER']
+	app.config['SECRET_KEY'] = environ['SECRET_KEY']
+	app.config['SQLALCHEMY_DATABASE_URI'] = environ['SQLALCHEMY_DATABASE_URI']
+
 db = SQLAlchemy(app)
 db.create_all()
  
