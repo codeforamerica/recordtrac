fed6e46f5682b5a70d87b42a7e89580cd61e02a0
Richa Agarwal <richa.agarwal85@gmail.com>
2013-05-21 17:04:29 -0700

    Adding back heroku config setup

--- public_records_portal/__init__.py
+++ public_records_portal/__init__.py
@@ -1,13 +1,14 @@
 from os import environ
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.heroku import Heroku
 
 # Initialize Flask app and database:
 app = Flask(__name__)
 try:
 	app.config.from_object('local_config')
 except:
-	pass # TODO: better way to set heroku config
+	heroku = Heroku(app) # Sets app.config from Heroku variables, that should be set already.
 db = SQLAlchemy(app)
 db.create_all()
  
