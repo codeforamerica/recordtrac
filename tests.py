e1b519e74cdcd7fe8feffe810b4cf4f250a0f470
Richa Agarwal <richa.agarwal85@gmail.com>
2013-06-05 17:35:04 -0700

    Adding a test for asking a question and automating login

--- tests.py
+++ tests.py
@@ -1,12 +1,20 @@
 import os
-from public_records_portal import app
+from public_records_portal.prflask import app
 import unittest
 import random, string
 
 
 class PublicRecordsTestCase(unittest.TestCase):
+
+	def login(self, username, password):
+		return self.app.post('/login', data=dict(
+        email=username,
+        password=password
+    ), follow_redirects=True)
+
 	def setUp(self):
 		self.app = app.test_client()
+		self.login('richa@codeforamerica.org', app.config['ADMIN_PASSWORD'])
 
 	def test_empty_db(self):
 		page = self.app.get('/requests')
@@ -17,9 +25,23 @@ class PublicRecordsTestCase(unittest.TestCase):
 		assert 'All requests' in page.data
 
 	def test_submit_request(self):
-		request = 'this is a new request and some random string %s' %''.join(random.choice(string.lowercase) for i in range(10))
-		page = self.submit_request('richa@richa.com', request)
+		page, request = self.make_random_request()
 		assert request in page.data
+
+	def test_home(self):
+		page = self.app.get('/')
+		print page
+		assert 'Submit a Request' in page.data
+
+	def make_random_request(self):
+		request = 'this is a new request and some random string %s' %''.join(random.choice(string.lowercase) for i in range(10))
+		return self.submit_request('richa@richa.com', request), request
+
+	def test_question(self):
+		question = 'this is a test question'
+		fields = dict(request_id = 33, user_id = 2, question_text = question)
+		page = self.submit_generic(fields = fields, endpoint = "add_a_qa")
+		assert 'richa@codeforamerica.org' in page.data
 
 	def test_submit_duplicate_request(self):
 		request = 'this is a duplicate request'
@@ -27,10 +49,13 @@ class PublicRecordsTestCase(unittest.TestCase):
 		assert 'You broke the internet' in page.data
 
 	def submit_request(self, email, text):
-		return self.app.post('/', data=dict(
+		return self.app.post('/new', data=dict(
 		request_text= text,
 		request_email = email
 	), follow_redirects=True)
+
+	def submit_generic(self, fields, endpoint = ""):
+		return self.app.post('/%s' % (endpoint), data = fields, follow_redirects= True)
 
 if __name__ == '__main__':
 	unittest.main()
