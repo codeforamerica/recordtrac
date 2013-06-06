6566fa084cf8112c632b0aaa2f21c880e6cd9e31
Richa Agarwal <richa.agarwal85@gmail.com>
2013-06-05 18:51:05 -0700

    App config might not be relevant

--- public_records_portal/__init__.py
+++ public_records_portal/__init__.py
@@ -10,21 +10,24 @@ app.debug = True
 try:
 	app.config.from_object('local_config')
 except:
-	app.config['ENVIRONMENT'] = environ['ENVIRONMENT']
-	app.config['APPLICATION_URL'] = environ['APPLICATION_URL']
-	app.config['SCRIBD_API_KEY'] = environ['SCRIBD_API_KEY']
-	app.config['SCRIPT_API_SECRET'] = environ['SCRIBD_API_SECRET']
-	app.config['DEFAULT_OWNER_NAME'] = environ['DEFAULT_OWNER_NAME']
-	app.config['DEFAULT_OWNER_EMAIL'] = environ['DEFAULT_OWNER_EMAIL']
-	app.config['DEFAULT_OWNER_REASON'] = environ['DEFAULT_OWNER_REASON']
-	app.config['MAIL_USERNAME'] = environ['MAIL_USERNAME']
-	app.config['MAIL_PASSWORD'] = environ['MAIL_PASSWORD']
-	app.config['DEFAULT_MAIL_SENDER'] = environ['DEFAULT_MAIL_SENDER']
-	app.config['SECRET_KEY'] = environ['SECRET_KEY']
-	app.config['SCRIBD_API_KEY'] = environ['SCRIBD_API_KEY']
-	app.config['SCRIBD_API_SECRET'] = environ['SCRIBD_API_SECRET']
-	app.config['HOST_URL'] = environ['HOST_URL']
-	app.config['SQLALCHEMY_DATABASE_URI'] = environ['SQLALCHEMY_DATABASE_URI']
+	try:
+		app.config['ENVIRONMENT'] = environ['ENVIRONMENT']
+		app.config['APPLICATION_URL'] = environ['APPLICATION_URL']
+		app.config['SCRIBD_API_KEY'] = environ['SCRIBD_API_KEY']
+		app.config['SCRIPT_API_SECRET'] = environ['SCRIBD_API_SECRET']
+		app.config['DEFAULT_OWNER_NAME'] = environ['DEFAULT_OWNER_NAME']
+		app.config['DEFAULT_OWNER_EMAIL'] = environ['DEFAULT_OWNER_EMAIL']
+		app.config['DEFAULT_OWNER_REASON'] = environ['DEFAULT_OWNER_REASON']
+		app.config['MAIL_USERNAME'] = environ['MAIL_USERNAME']
+		app.config['MAIL_PASSWORD'] = environ['MAIL_PASSWORD']
+		app.config['DEFAULT_MAIL_SENDER'] = environ['DEFAULT_MAIL_SENDER']
+		app.config['SECRET_KEY'] = environ['SECRET_KEY']
+		app.config['SCRIBD_API_KEY'] = environ['SCRIBD_API_KEY']
+		app.config['SCRIBD_API_SECRET'] = environ['SCRIBD_API_SECRET']
+		app.config['HOST_URL'] = environ['HOST_URL']
+		app.config['SQLALCHEMY_DATABASE_URI'] = environ['SQLALCHEMY_DATABASE_URI']
+	except:
+		print "Config variables were not set"
 
 db = SQLAlchemy(app)
 from public_records_portal import models
