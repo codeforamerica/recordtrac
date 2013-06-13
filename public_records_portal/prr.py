2e2a828e7b50db8dd3008832d5231262cd2051c3
Richa Agarwal <richa.agarwal85@gmail.com>
2013-06-13 13:37:47 -0700

    Update owner automatically if logged in user is performing an action and is not the current owner

--- public_records_portal/prr.py
+++ public_records_portal/prr.py
@@ -8,6 +8,7 @@ import time
 import requests as seaturtle # HTTP requests library, renaming because it's confusing otherwise
 import json
 from flask import Flask, render_template, request
+from flask.ext.login import current_user
 from werkzeug import secure_filename
 import sendgrid
 from datetime import datetime
@@ -95,6 +96,7 @@ def update_resource(resource, request_body):
 		answer_a_question(fields['qa_id'], fields['answer_text'])
 		return True
 	elif "owner" in resource:
+		change_request_status(fields['request_id'], "Rerouted")
 		assign_owner(fields['request_id'], fields['owner_reason'], fields['owner_email'])
 	else:
 		return False
@@ -211,11 +213,15 @@ def assign_owner(request_id, reason, email = None, alias = None, phone = None):
 	else:
 		past_owner_id = None
 	put_resource("request", dict(current_owner = new_owner['id']) ,int(request_id))
-	change_request_status(request_id, "Pending")
 	send_prr_email(request_id = request_id, notification_type = "Request assigned", owner_id = new_owner['id'])
 	return past_owner_id, new_owner['id']
 
 def change_request_status(request_id, status):
+	req = get_resource("request", request_id)
+	if current_user.is_anonymous() == False:
+		current_owner = get_resource("owner", req['current_owner'])
+		if current_user.id != current_owner['user_id']:
+			assign_owner(request_id = request_id, reason = "Self assigned", email = current_user.email, alias = current_user.alias, phone = current_user.phone)
 	return put_resource("request", dict(status = status, status_updated = datetime.now().isoformat()),int(request_id))
 
 def progress(bytes_sent, bytes_total):
