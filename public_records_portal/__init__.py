5a2d7f7e230ced5d989c6702487461f9719c6b53
Richa Agarwal <richa.agarwal85@gmail.com>
2013-07-23 20:55:22 -0700

    Setting app.config from environ now

--- public_records_portal/__init__.py
+++ public_records_portal/__init__.py
@@ -13,30 +13,38 @@ import flask.ext.restless
 # Initialize Flask app and database:
 app = Flask(__name__)
 app.debug = True
-app.config['SECRET_KEY'] = "Not really a secret."
-app.config['ENVIRONMENT'] = "LOCAL"
-app.config['DEFAULT_OWNER_REASON'] = "Default owner reason"
-app.config['DEFAULT_OWNER_EMAIL'] = "Default owner email"
 
-try:
-	app.config.from_object('local_config')
-except:
-	try:
-		app.config['SECRET_KEY'] = environ['SECRET_KEY']
-		app.config['ENVIRONMENT'] = environ['ENVIRONMENT']
-		app.config['APPLICATION_URL'] = environ['APPLICATION_URL']
-		app.config['SCRIBD_API_KEY'] = environ['SCRIBD_API_KEY']
-		app.config['SCRIBD_API_SECRET'] = environ['SCRIBD_API_SECRET']
-		app.config['DEFAULT_OWNER_EMAIL'] = environ['DEFAULT_OWNER_EMAIL']
-		app.config['DEFAULT_OWNER_REASON'] = environ['DEFAULT_OWNER_REASON']
-		app.config['MAIL_USERNAME'] = environ['MAIL_USERNAME']
-		app.config['MAIL_PASSWORD'] = environ['MAIL_PASSWORD']
-		app.config['ADMIN_PASSWORD'] = environ['ADMIN_PASSWORD']
-		app.config['DEFAULT_MAIL_SENDER'] = environ['DEFAULT_MAIL_SENDER']
-		app.config['HOST_URL'] = environ['HOST_URL']
-		app.config['SQLALCHEMY_DATABASE_URI'] = environ['SQLALCHEMY_DATABASE_URI']
-	except:
-		print "Config variables were not set"
+def set_env(key, default):
+	if key in environ:
+		app.config[key] = environ[key]
+	else:
+		app.config[key] = default
+
+# You can set these in a file such as settings.env. See settings.env.example
+
+set_env(key = 'ENVIRONMENT', default="LOCAL")
+
+set_env(key = 'DEFAULT_OWNER_REASON', default = 'The reason the default owner gets assigned a request' )
+set_env(key = 'DEFAULT_OWNER_EMAIL', default = 'citystaff@city.gov')
+# If you set up Sendgrid...
+set_env(key = 'DEFAULT_MAIL_SENDER', default = 'appemail@app.com') 
+set_env(key = 'MAIL_USERNAME', default='Oakland Public Records')
+set_env(key = 'MAIL_PASSWORD', default = "")
+
+# For app logins
+set_env(key = 'ADMIN_PASSWORD', default = "NotSoSecretPassword")
+
+# Flask app secret key
+set_env(key = 'SECRET_KEY', default = "NotSoSecretKey")
+
+set_env(key = 'APPLICATION_URL', default = "http://127.0.0.1:5000/")
+
+# For Scribd uploads...
+set_env(key = 'SCRIBD_API_KEY', default = "")
+set_env(key = 'SCRIBD_API_SECRET', default = "")
+
+set_env(key = 'SQLALCHEMY_DATABASE_URI', default = "postgresql://localhost/publicrecords")
+set_env(key = 'HOST_URL', default = 'https://www.scribd.com/doc/')
 
 
 db = SQLAlchemy(app)
@@ -44,6 +52,7 @@ from public_records_portal import models
 from models import *
 
 # Create API
+# Maybe move this over to prflask and comment on how this sets up the URLs
 manager = APIManager(app, flask_sqlalchemy_db=db)
 manager.create_api(Request, methods=['GET', 'POST', 'PUT', 'DELETE'], results_per_page=None)
 manager.create_api(Owner, methods=['GET', 'POST', 'PUT', 'DELETE'], results_per_page = None)
