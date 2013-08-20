97b67156df6b3cec5ef38088d57e7bf6770c0ffc
Mike Holubowski <mike.holubowski@gmail.com>
2013-08-20 10:38:38 -0700

    Changed buttons to include short desc

--- public_records_portal/static/js/_response_widget.js
+++ public_records_portal/static/js/_response_widget.js
@@ -4,18 +4,18 @@
   var $rw_btns_wrap = $rw_wrap.find('.rw-controller-btns-container');
   var $rw_actions_wrap = $rw_wrap.find('.rw-actions-container');
 
-  $rw_btns_wrap.on('click', '.rw-btn', function(){
+  $rw_btns_wrap.on('click', '.rw-btn-wrap', function(){
     $this = $(this);
     target = $this.data('target');
     // active tab background transitions
     $rw_btns_wrap.find('.rw-btn-wrap').each(function(){
       $(this).removeClass('active');
     });
-    $this.parent().addClass('active');
+    $this.addClass('active');
     // toggle content (data-target)
     $rw_actions_wrap.find('[data-target-for]').each(function(){
       $(this).hide();
-    })
+    });
     $rw_actions_wrap.find('[data-target-for="'+target+'"]').show('400');
   });
 
