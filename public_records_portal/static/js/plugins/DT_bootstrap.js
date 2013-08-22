6045934298b28454eaf9ad1b44eb238ce1739158
criscristina <cris@codeforamerica.org>
2013-08-22 13:55:12 -0700

    set default request shown from 10 to 50

--- public_records_portal/static/js/plugins/DT_bootstrap.js
+++ public_records_portal/static/js/plugins/DT_bootstrap.js
@@ -153,6 +153,7 @@ $(document).ready(function() {
 		"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
 		"sPaginationType": "bootstrap",
 		"aaSorting": [[ 0, "desc" ]],
+		"iDisplayLength": 50,
 		"oLanguage": {
 			"sLengthMenu": "_MENU_ records per page"
 		}
