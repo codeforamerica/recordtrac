155c5a6478c0f33ad5eeff038249914ab1f498c6
90fcf9498f465f0090235a17a824abf584eee90b <richa.agarwal85@gmail.com>
2013-09-11 13:20:31 -0700

    Reverting back to earlier new_request.js

--- public_records_portal/static/js/new_request.js
+++ public_records_portal/static/js/new_request.js
@@ -2,6 +2,7 @@
   $(document).ready(function(){
 
     $("#submitRequest").validate({
+      // ignore: ".ignore",
       rules: {
         request_text: {
           required: true,
@@ -9,9 +10,26 @@
              }
          },
       highlight: function(element) {
+          // $('#inputEmail').addClass('ignore');
+          // $('#requestButton').html(
+          //   "<div id='no_email' class='alert alert-block alert-error fade in'>"+
+          //     "<h4 class='alert-heading'>Oops, you forgot to provide an email address.</h4>"+
+          //       "<p>Your email is how we contact you.  If you choose to leave it blank, you will not receive updates about your request.  Your request may take longer or be dismissed if it is missing important information. You can still enter your e-mail address at this time, and continue with submission.</p>"+
+          //       "<button id='submit_anyway' class='btn btn-danger' type='submit'>Got it, submit my request!</button>"+
+          //     "</div>"
+          //   );
         $(element).closest('.control-group').removeClass('success').addClass('error');
         },
       success: function(element) {
+        // $('#requestButton').html(
+        //   "<div id='requestButton' class='control-group'>"+
+        //     "<div class='controls'>"+
+        //       "<button type='submit' class='btn btn-large btn-primary'>Request</button>"+
+        //     "</div>"+
+        //   "</div>"
+        //   );
+        element
+        // .text('OK!').addClass('valid')
         .closest('.control-group').removeClass('error').addClass('success');
         }
     });
@@ -65,7 +83,7 @@ $('#inputDepartment').typeahead({
       if (data != '') {
         $div.addClass('alert').addClass('alert-error');
         $div.html(data);
-        $div.prepend("<i class='icon-exclamation-sign'></i>");
+        $div.prepend("<i class='icon-exclamation-sign'></i> ");
       } else {
         $div.empty();
         $div.removeClass('alert').removeClass('alert-error');
