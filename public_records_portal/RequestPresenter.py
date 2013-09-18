e98b545205db81380c1ec9fa8a01f8b71a815daf
criscristina <cris@codeforamerica.org>
2013-09-18 13:58:59 -0700

    updated input on public page for note and question

--- public_records_portal/RequestPresenter.py
+++ public_records_portal/RequestPresenter.py
@@ -30,7 +30,13 @@ class RequestPresenter:
 				text = text + "<p>%s - <em>Requester</em></p>" %(self.response.answer)
 			else:
 				if self.public:
-					text = text + "<form name='respond_question' class='form-inline' id='answer' method='post' action='/update_a_qa' autocomplete='on'><label class='control-label'>Answer</label><input type='hidden' name='qa_id' value='%s'/><input type='hidden' name='request_id' value='%s'/><textarea id='answerTextarea' name='answer_text' class='input-xxlarge' type='text' rows='1' placeholder='Can you respond to the above question?' required/></textarea><button id='askQuestion' class='btn btn-primary' type='submit'>Respond</button></form>" % (self.response.id, self.request.id)
+					text = text + """
+					<form name='respond_question' class='form-inline' id='answer' method='post' action='/update_a_qa' autocomplete='on'>
+						<label class='control-label'>Answer</label><input type='hidden' name='qa_id' value='%s'/><input type='hidden' name='request_id' value='%s'/>
+						<textarea id='answerTextarea' name='answer_text' class='input-xlarge' rows="2" type='text' rows='1' placeholder='Can you respond to the above question?' required/></textarea>
+						<button id='askQuestion' class='btn btn-primary' type='submit'>Respond</button>
+					</form> 
+					""" % (self.response.id, self.request.id)
 				else:
 					text = text + "<p>Requester hasn't answered yet.</p>"
 			return text
