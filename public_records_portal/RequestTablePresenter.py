5b138294b4a561cfb2a74c2d76c5e78ef297b29b
90fcf9498f465f0090235a17a824abf584eee90b <richa.agarwal85@gmail.com>
2013-10-09 15:28:32 -0700

    Updating status field

--- public_records_portal/RequestTablePresenter.py
+++ public_records_portal/RequestTablePresenter.py
@@ -16,23 +16,27 @@ class RequestTablePresenter:
 				self.status, self.due_date = status_arr[0], date(status_arr[1])
 			else:
 				self.status, self.due_date = None, None
+		self.text = request.text
+		if len(self.text) > 140:
+			self.text = '%s...' % self.text[:140]
 		if self.status == "open":
 			self.color = "#2688AD"
 		elif self.status == "closed":
 			self.color = "#2a2b2b"
 		elif self.status == "due soon":
 			self.color = "#FB991B"
-		else:
+			self.text = '<span style="background-color: %s" class="label label-warning">%s</span> %s' %(self.color, self.status, self.text)
+		elif self.status == "overdue":
 			self.color = "#CA1A1A"
-		self.text = request.text
-		if len(self.text) > 140:
-			self.text = '%s...' % self.text[:140]
+			self.text = '<span style="background-color: %s" class="label label-important">%s</span> %s' %(self.color, self.status, self.text)
+		else:
+			self.color=""
 		self.status_icon = get_status_icon(self.status)
 		self.department = get_dept_by_request(request)
 		self.point_of_contact = get_owner_data(request.id, attributes = ["alias"])[0]
 		if public:
-			self.display_text = "<td class='status' bgcolor='%s'><small><i class='%s'></i></small>%s</td><td>%s</td><td>%s</td><td><div>%s</div></td><td>%s</td><td>%s</td><td style='display:none;'>%s</td>" %(self.color,self.status_icon,self.status,request.id, date(request.date_created), self.text, self.department, self.point_of_contact, request.text)
+			self.display_text = "<td class='status' bgcolor='%s'><small><i class='%s'></i></small></td><td>%s</td><td>%s</td><td><div>%s</div></td><td>%s</td><td>%s</td><td style='display:none;'>%s</td>" %(self.color,self.status_icon,request.id, date(request.date_created), self.text, self.department, self.point_of_contact, request.text)
 		else:
-			self.display_text = "<td class='status' bgcolor='%s'><small><i class='%s'></i></small>%s</td><td>%s</td><td>%s</td><td><div>%s</div></td><td>%s</td><td>%s</td><td>%s</td><td style='display:none;'>%s</td>" %(self.color,self.status_icon,self.status,request.id, date(request.date_created), self.text, self.department, self.point_of_contact, (self.due_date or "N/A"), request.text)
+			self.display_text = "<td class='status' bgcolor='%s'><small><i class='%s'></i></small></td><td>%s</td><td>%s</td><td><div>%s</div></td><td>%s</td><td>%s</td><td>%s</td><td style='display:none;'>%s</td>" %(self.color,self.status_icon,request.id, date(request.date_created), self.text, self.department, self.point_of_contact, (self.due_date or "N/A"), request.text)
 			
 
