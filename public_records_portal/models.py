bebfb03c063ae3da198f9ab17370f970ad7ca9e4
90fcf9498f465f0090235a17a824abf584eee90b <richa.agarwal85@gmail.com>
2013-10-10 17:49:53 -0700

    Added viz table

--- public_records_portal/models.py
+++ public_records_portal/models.py
@@ -160,3 +160,17 @@ class Note(db.Model):
 	def __repr__(self):
 		return '<Note %r>' % self.text
 
+### @export "Visualization"
+class Visualization(db.Model):
+	__tablename__ = 'visualization'
+	id = db.Column(db.Integer, primary_key = True)
+	content = db.Column(db.String())
+	date_created = db.Column(db.DateTime)
+	date_updated = db.Column(db.DateTime)
+	type_viz = db.Column(db.String())
+	def __init__(self, type_viz, content):
+		self.type_viz = type_viz
+		self.content = content
+		self.date_created = datetime.now().isoformat()
+	def __repr__(self):
+		return '<Visualization %r>' % self.type_viz
