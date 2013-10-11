e71490f6a11c50e2c153cd148a2d9fcbcbf01fa9
90fcf9498f465f0090235a17a824abf584eee90b <richa.agarwal85@gmail.com>
2013-10-10 22:15:04 -0700

    Adding create_viz_data to cron job

--- cron_jobs.py
+++ cron_jobs.py
@@ -1,6 +1,7 @@
 from public_records_portal.notifications import notify_due
 from public_records_portal.departments import populate_users_with_departments
 from public_records_portal.prr import set_directory_fields
+from public_records_portal.db_helpers import create_viz_data
 
 # Notify city staff via e-mail if they belong to a request that is due soon or overdue:
 notify_due()
@@ -11,3 +12,5 @@ set_directory_fields()
 # Update user info with information in the departments.json file
 populate_users_with_departments()
 
+# Create visualizations
+create_viz_data()
