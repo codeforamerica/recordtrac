8ae6146961049bb579c7f8045af5fa4780cf7299
Ryan Txanson <ryan.txanson@gmail.com>
2014-02-09 20:50:15 +0000

    Fixed typo: Mozzila -> Mozilla

--- public_records_portal/spam.py
+++ public_records_portal/spam.py
@@ -6,7 +6,7 @@ def is_spam(comment):
 	blog = app.config['APPLICATION_URL']
 	if isinstance(comment, unicode):
 		comment = comment.encode('utf8', 'ignore')
-	if akismet.comment_check(key = key, blog = blog, user_ip = '127.0.0.1', user_agent = 'Mozzila/5.0 (...) Gecko/20051111 Firefox/1.5', comment_content = comment) or 'http' in comment:
+	if akismet.comment_check(key = key, blog = blog, user_ip = '127.0.0.1', user_agent = 'Mozilla/5.0 (...) Gecko/20051111 Firefox/1.5', comment_content = comment) or 'http' in comment:
 		return True
 	return False
 
