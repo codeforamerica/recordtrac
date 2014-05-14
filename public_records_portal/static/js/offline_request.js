b0f5110a9c49753a53a2cb6d5ebc603595c77ff8
Richa Agarwal <richa@Richas-MacBook-Air.local>
2014-05-14 14:41:30 -0700

    Renamed this file

--- public_records_portal/static/js/offline_request.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-  $('#datepicker').datepicker();
-  var currentDate = new Date();
-  currentDay = (currentDate.getMonth()+1) + '/' + currentDate.getDate() + '/' + currentDate.getFullYear();
-  $('#datepicker').val(currentDay);
-
-});
