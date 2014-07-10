53267a86e6d7063c836164b2aff395763d666297
Richa Agarwal <richa.agarwal85@gmail.com>
2014-07-09 17:32:05 -0700

    Cleanup

--- public_records_portal/departments.py
+++ public_records_portal/departments.py
@@ -28,19 +28,6 @@ def create_doctypes():
 	with open(os.path.join(app.root_path, 'static/json/doctypes.json'), 'w') as outfile:
   		json.dump(depts, outfile)
 
-### @export "create_list_depts"
-def create_list_depts():
-	depts = []
-	depts_json = open(os.path.join(app.root_path, 'static/json/departments.json'))
-	json_data = json.load(depts_json)
-	for department in json_data:
-		d = Department.query.filter_by(name = department).first()
-		if not d:
-			d = Department(name = department)
-			put_obj(d)
-		depts.append(department)
-  	with open(os.path.join(app.root_path, 'static/json/list_of_departments.json'), 'w') as outfile:
-  		json.dump(depts, outfile)
 
 ### @export "get_dept_backup"
 def get_dept_backup(dept_contact):
@@ -74,22 +61,3 @@ def get_depts(user):
 				depts_backup.append(line)
 	return depts_contact, depts_backup
 
-### @export "populate_users_with_departments"
-# This is for PRR Liaisons
-def populate_users_with_departments():
-	users = get_objs("User")
-	for u in users:
-		department_name = get_dept(u)
-		if not department_name:
-			continue
-		dept = Department.query.filter_by(name = department_name).first()
-		if not dept:
-			dept = Department(name = department_name)
-			put_obj(dept)
-		if dept:
-			update_obj(attribute = "department", val = dept.id, obj = u)
-		depts_contact, depts_backup = get_depts(u)
-		if depts_contact:
-			update_obj(attribute = "contact_for", val = ",".join(depts_contact), obj = u)
-		if depts_backup:
-			update_obj(attribute = "backup_for", val = ",".join(depts_backup), obj = u)
