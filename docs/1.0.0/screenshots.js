8a96b40545908324b565911904277bc1c337543d
Richa Agarwal <richa.agarwal85@gmail.com>
2014-09-08 14:35:21 -0700

    Adding 1.0.0 docs back in

---
+++ docs/1.0.0/screenshots.js
@@ -0,0 +1,33 @@
+[source,js]
+----
+var casper = require('casper').create({
+    viewportSize : {width : 800, height : 600},
+    verbose: true,
+    logLevel: "debug",
+});
+
+casper.start("http://localhost:3000", function userInterface() {
+    this.capture("index.png[]");
+});
+
+casper.thenOpen('http://records.oaklandnet.com/request/1', function userInterface() {
+    this.captureSelector("request1response.png[]", "#responses-container");
+    this.captureSelector("request1request.png[]", "#request-container");
+ });
+
+// casper.thenOpen('http://records.oaklandnet.com/public/request/954', function userInterface() {
+//     this.captureSelector("public_add_a_note.png[]", "#public_note");
+//  });
+
+casper.thenOpen('http://records.oaklandnet.com/new', function userInterface() {
+    this.captureSelector("new_request_form.png[]", "#submitRequest");
+ });
+
+casper.thenOpen('http://records.oaklandnet.com/api/request/1', function userInterface() {
+	var fs = require('fs');
+    fs.write('api.json', this.getPageContent());
+ });
+
+
+casper.run();
+----
