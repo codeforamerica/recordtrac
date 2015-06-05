9342d66ddae6b7544212b8cf56f3192e75f5a229
Brandon Tang <blt264@nyu.edu>
2015-06-05 14:41:19 -0400

    Fixed Date Receieved Field

--- public_records_portal/static/js/date_picker.js
+++ public_records_portal/static/js/date_picker.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 
   $("input[id^='datepicker']").datepicker();
-  var currentDate = new Date();
-  currentDay = (currentDate.getMonth()+1) + '/' + currentDate.getDate() + '/' + currentDate.getFullYear();
-  $("input[id^='datepicker']").val(currentDay);
+  $("input[id^='datepicker']").val('');
 
 });
