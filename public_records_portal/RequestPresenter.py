b435c7402ba66937980d8736732ea3ea6cc65e22
Joel Castillo <jcastillo@records.nyc.gov>
2015-06-11 09:48:42 -0400

    Fixed formatting

--- public_records_portal/RequestPresenter.py
+++ public_records_portal/RequestPresenter.py
@@ -11,68 +11,74 @@ from public_records_portal import models
 from models import Note, QA
 from db_helpers import get_obj
 
-class RequestPresenter:
-	def __init__(self, request, qa = None, note = None, index = None, public = False):
-		self.index = index
-		self.public = public
-		self.request = request
-		if qa:
-			self.response = qa
-			self.type = "qa"
-			self.uid = self.response.owner_id
-			self.staff = get_obj("User", self.uid)
-			self.staff_email = "N/A"
-			self.staff_department = "N/A"
-			self.staff_phone = "N/A"
-			self.staff_alias = "N/A"
-			if self.staff:
-				if self.staff.email:
-					self.staff_email = self.staff.email
-				if self.staff.department:
-					self.staff_department = self.staff.department
-				if self.staff.phone:
-					self.staff_phone = self.staff.phone
-				if self.staff.alias:
-					self.staff_alias = self.staff.alias
-			directory_popover = "directoryPopover('%s', '%s', '%s', '#contactinfoPopoverQA%s')" %(self.staff_email, self.staff_department, self.staff_phone, index)
-			self.owner_link = '<a href="/staff_card/%s" data-placement="top" data-toggle="popover" href="#" id="contactinfoPopoverQA%s" class="hidden-phone hidden-tablet"><span class="contactinfoPopover" onmouseover="%s">%s</span></a>' % (self.response.owner_id, index, directory_popover, self.staff_alias or self.staff_email)
-			self.icon = "icon-question icon-large"
-		if note:
-			self.response = note
-			self.type = "note"
-			self.icon = "icon-edit icon-large"
-	
-	def get_id(self):
-		return self.response.id
 
-	def display_text(self):
-		if self.type == "qa":
-			text = "%s - %s" %(self.response.question, self.owner_link)
-			if self.response.answer:
-				text = text + "<p>%s - <span class='requester'>Requester</span></p>" %(self.response.answer)
-			else:
-				if self.request.is_closed():
-					text = text + "<i><p>No response from requester</p></i>"
-				else:
-					if self.public:
-						text = text + """
+class RequestPresenter:
+
+    def __init__(self, request, qa=None, note=None, index=None, public=False):
+        self.index = index
+        self.public = public
+        self.request = request
+        if qa:
+            self.response = qa
+            self.type = "qa"
+            self.uid = self.response.owner_id
+            self.staff = get_obj("User", self.uid)
+            self.staff_email = "N/A"
+            self.staff_department = "N/A"
+            self.staff_phone = "N/A"
+            self.staff_alias = "N/A"
+            if self.staff:
+                if self.staff.email:
+                    self.staff_email = self.staff.email
+                if self.staff.department:
+                    self.staff_department = self.staff.department
+                if self.staff.phone:
+                    self.staff_phone = self.staff.phone
+                if self.staff.alias:
+                    self.staff_alias = self.staff.alias
+            directory_popover = "directoryPopover('%s', '%s', '%s', '#contactinfoPopoverQA%s')" % (
+                self.staff_email, self.staff_department, self.staff_phone, index)
+            self.owner_link = '<a href="/staff_card/%s" data-placement="top" data-toggle="popover" href="#" id="contactinfoPopoverQA%s" class="hidden-phone hidden-tablet"><span class="contactinfoPopover" onmouseover="%s">%s</span></a>' % (
+                self.response.owner_id, index, directory_popover, self.staff_alias or self.staff_email)
+            self.icon = "icon-question icon-large"
+        if note:
+            self.response = note
+            self.type = "note"
+            self.icon = "icon-edit icon-large"
+
+    def get_id(self):
+        return self.response.id
+
+    def display_text(self):
+        if self.type == "qa":
+            text = "%s - %s" % (self.response.question, self.owner_link)
+            if self.response.answer:
+                text = text + \
+                    "<p>%s - <span class='requester'>Requester</span></p>" % (
+                        self.response.answer)
+            else:
+                if self.request.is_closed():
+                    text = text + "<i><p>No response from requester</p></i>"
+                else:
+                    if self.public:
+                        text = text + """
 						<form name='respond_question' class='form-inline' id='answer' method='post' action='/update_a_qa' autocomplete='on'>
 							<label class='control-label'>Answer</label><input type='hidden' name='qa_id' value='%s'/><input type='hidden' name='request_id' value='%s'/>
 							<textarea id='answerTextarea' name='answer_text' class='input-xlarge' rows="2" type='text' rows='1' placeholder='Can you respond to the above question?' required/></textarea>
 							<button id='askQuestion' class='btn btn-primary' type='submit'>Respond</button>
 						</form> 
 						""" % (self.response.id, self.request.id)
-					else:
-						text = text + "<p>Requester hasn't answered yet.</p>"
-			return text
-		elif self.type == "note":
-			return "%s - Requester" %(self.response.text)
-		
-	def get_icon(self):
-		return self.icon
+                    else:
+                        text = text + "<p>Requester hasn't answered yet.</p>"
+            return text
+        elif self.type == "note":
+            return "%s - Requester" % (self.response.text)
 
-	def set_icon(self, icon):
-		self.icon = icon
+    def get_icon(self):
+        return self.icon
 
-	def date(self):
-		return self.response.date_created
+    def set_icon(self, icon):
+        self.icon = icon
+
+    def date(self):
+        return self.response.date_created
--- public_records_portal/scribd_helpers.py
+++ public_records_portal/scribd_helpers.py
@@ -24,11 +24,14 @@ def should_upload():
 
 
 # These are the extensions that can be uploaded to Scribd.com:
-ALLOWED_EXTENSIONS = ['txt', 'pdf', 'doc', 'ps', 'rtf', 'epub', 'key', 'odt', 'odp', 'ods', 'odg', 'odf', 'sxw', 'sxc', 'sxi', 'sxd', 'ppt', 'pps', 'xls', 'zip', 'docx', 'pptx', 'ppsx', 'xlsx', 'tif', 'tiff']
+ALLOWED_EXTENSIONS = ['txt', 'pdf', 'doc', 'ps', 'rtf', 'epub', 'key', 'odt', 'odp', 'ods', 'odg', 'odf',
+                      'sxw', 'sxc', 'sxi', 'sxd', 'ppt', 'pps', 'xls', 'zip', 'docx', 'pptx', 'ppsx', 'xlsx', 'tif', 'tiff']
 
 
 def progress(bytes_sent, bytes_total):
-    app.logger.info("Scribd upload in progress: %s of %s (%s%%)" % (bytes_sent, bytes_total, bytes_sent*100/bytes_total))
+    app.logger.info("Scribd upload in progress: %s of %s (%s%%)" %
+                    (bytes_sent, bytes_total, bytes_sent*100/bytes_total))
+
 
 def upload(document, filename, API_KEY, API_SECRET, description):
     app.logger.info("\n\nuploading file")
@@ -38,49 +41,57 @@ def upload(document, filename, API_KEY, API_SECRET, description):
     try:
         # Upload the document from a file.
         doc = scribd.api_user.upload(
-            targetfile = document,
-            name = filename,
+            targetfile=document,
+            name=filename,
             progress_callback=progress,
-            req_buffer = tempfile.TemporaryFile()
-            )  
-        doc.description = description    
-        doc.save() 
+            req_buffer=tempfile.TemporaryFile()
+        )
+        doc.description = description
+        doc.save()
         doc_id = doc.id
         return doc_id
     except scribd.ResponseError, err:
-        app.logger.info('Scribd failed: code=%d, error=%s' % (err.errno, err.strerror))
+        app.logger.info('Scribd failed: code=%d, error=%s' %
+                        (err.errno, err.strerror))
         return err.strerror
 
-def get_scribd_download_url(doc_id, record_id = None):
+
+def get_scribd_download_url(doc_id, record_id=None):
     if not should_upload():
         return None
-	API_KEY = app.config['SCRIBD_API_KEY']
-	API_SECRET = app.config['SCRIBD_API_SECRET']
-	try:
-		scribd.config(API_KEY, API_SECRET)
-		doc = scribd.api_user.get(doc_id)
-		doc_url = doc.get_download_url()
-		if record_id:
-			set_scribd_download_url(download_url = doc_url, record_id = record_id)
-		return doc_url
-	except:
-		return None
+        API_KEY = app.config['SCRIBD_API_KEY']
+        API_SECRET = app.config['SCRIBD_API_SECRET']
+        try:
+            scribd.config(API_KEY, API_SECRET)
+            doc = scribd.api_user.get(doc_id)
+            doc_url = doc.get_download_url()
+            if record_id:
+                set_scribd_download_url(
+                    download_url=doc_url, record_id=record_id)
+            return doc_url
+        except:
+            return None
 
 
 def set_scribd_download_url(download_url, record_id):
-    update_obj('download_url', download_url, obj_type = 'Record', obj_id = record_id)
+    update_obj(
+        'download_url', download_url, obj_type='Record', obj_id=record_id)
 
-def scribd_batch_download(): 
-	req = Request.query.all()
-	for record in req.records:
-		if record.download_url:
-			urllib.urlretrieve(record.downlaod_url, "saved_records/%s" %(record.filename))
+
+def scribd_batch_download():
+    req = Request.query.all()
+    for record in req.records:
+        if record.download_url:
+            urllib.urlretrieve(
+                record.downlaod_url, "saved_records/%s" % (record.filename))
+
 
 def make_public(doc_id, API_KEY, API_SECRET):
     scribd.config(API_KEY, API_SECRET)
     doc = scribd.api_user.get(doc_id)
     doc.access = 'public'
     doc.save()
+
 
 def make_private(doc_id, API_KEY, API_SECRET):
     scribd.config(API_KEY, API_SECRET)
@@ -92,56 +103,63 @@ def make_private(doc_id, API_KEY, API_SECRET):
 def update_descriptions(API_KEY, API_SECRET):
     scribd.config(API_KEY, API_SECRET)
     for doc in scribd.api_user.all():
-        record = models.Record.query.filter_by(doc_id = doc.id).first()
+        record = models.Record.query.filter_by(doc_id=doc.id).first()
         if record:
-            link_back = app.config['APPLICATION_URL'] + 'request/' + str(record.request_id)
-            description =  "This document was uploaded via RecordTrac in response to a public records request for the %s. You can view the original request here: %s" % ( app.config['AGENCY_NAME'], link_back)
+            link_back = app.config['APPLICATION_URL'] + \
+                'request/' + str(record.request_id)
+            description = "This document was uploaded via RecordTrac in response to a public records request for the %s. You can view the original request here: %s" % (
+                app.config['AGENCY_NAME'], link_back)
             doc.description = description
             doc.save()
-            app.logger.info("\n\nUpdated Scribd document %s's description to %s" %(doc.id, description))
-
+            app.logger.info(
+                "\n\nUpdated Scribd document %s's description to %s" % (doc.id, description))
 
 
 #@timeout(seconds=20)
-def upload_file(document, request_id): 
-# Uploads file to scribd.com and returns doc ID. File can be accessed at scribd.com/doc/id
+def upload_file(document, request_id):
+    # Uploads file to scribd.com and returns doc ID. File can be accessed at
+    # scribd.com/doc/id
     app.logger.info("\n\nscribd upload file")
     if not should_upload():
         app.logger.info("\n\nshoud not upload file")
-        return '1', None # Don't need to do real uploads locally
+        return '1', None  # Don't need to do real uploads locally
     if document:
         app.logger.info("\n\nbegin file upload")
         allowed = allowed_file(document.filename)
-        app.logger.info("\n\n%s is allowed: %s" %(document.filename, allowed[0]))
+        app.logger.info("\n\n%s is allowed: %s" %
+                        (document.filename, allowed[0]))
         if allowed[0]:
             filename = secure_filename(document.filename)
-            app.logger.info("\n\nfilename after secure_filename: %s" %(filename))
-            link_back = app.config['APPLICATION_URL'] + 'request/' + str(request_id)
-            app.logger.info("\n\nlink_back: %s" %(link_back));
+            app.logger.info(
+                "\n\nfilename after secure_filename: %s" % (filename))
+            link_back = app.config[
+                'APPLICATION_URL'] + 'request/' + str(request_id)
+            app.logger.info("\n\nlink_back: %s" % (link_back))
             #doc_id = upload(document = document, filename = filename, API_KEY = app.config['SCRIBD_API_KEY'], API_SECRET = app.config['SCRIBD_API_SECRET'], description = "This document was uploaded via RecordTrac in response to a public records request for the %s. You can view the original request here: %s" % (app.config['AGENCY_NAME'], link_back))
-            #return doc_id, filename
-            
+            # return doc_id, filename
+
             upload_path = upload_file_locally(document, filename, request_id)
             return upload_path, filename
-            
+
         else:
-            return allowed # Returns false and extension
+            return allowed  # Returns false and extension
     return None, None
-    
+
+
 def upload_file_locally(document, filename, request_id):
     app.logger.info("\n\nuploading file locally")
-    app.logger.info("\n\n%s" %(document))
-    
+    app.logger.info("\n\n%s" % (document))
+
     upload_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-    app.logger.info("\n\nupload path: %s" %(upload_path))
-    
+    app.logger.info("\n\nupload path: %s" % (upload_path))
+
     document.save(upload_path)
     app.logger.info("\n\nfile uploaded to local successfully")
-    
+
     return upload_path
 
 
-### @export "allowed_file"
+# @export "allowed_file"
 def allowed_file(filename):
     ext = filename.rsplit('.', 1)[1]
     return ext in ALLOWED_EXTENSIONS, ext
