3650d98c6c7c9096ae94aedce16036ff46956f72
Joel Castillo <jcastillo@records.nyc.gov>
2015-08-25 17:56:17 -0400

    Updated requirements.txt and install-clamav-osx

--- scripts/install-clamav-osx.sh
+++ scripts/install-clamav-osx.sh
@@ -1,5 +1,7 @@
 brew install clamav
+cp /usr/local/etc/clamav/freshclam.conf.sample /usr/local/etc/clamav/freshclam.conf
 sed -i -e "s/^Example/#Example/" /usr/local/etc/clamav/freshclam.conf
+cp /usr/local/etc/clamav/clamd.conf.sample /usr/local/etc/clamav/clamd.conf
 sed -i -e "s/^Example/#Example/" /usr/local/etc/clamav/clamd.conf
 sed -i -e "s/^#TCPSocket/TCPSocket/" /usr/local/etc/clamav/clamd.conf
 freshclam
