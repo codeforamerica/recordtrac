b8abd5c7b26a1fbf6760e86bc0fd8ee45038bffc
Joel Castillo <jcastillo@records.nyc.gov>
2015-09-20 19:23:52 -0400

    Applied design to login form

--- public_records_portal/views.py
+++ public_records_portal/views.py
@@ -1033,6 +1033,8 @@ def login():
     form = LoginForm()
     errors = []
     if request.method == 'POST':
+        print form.username.data
+        print form.password.data
         if form.validate_on_submit():
             user_to_login = authenticate_login(form.username.data, form.password.data)
             if user_to_login:
