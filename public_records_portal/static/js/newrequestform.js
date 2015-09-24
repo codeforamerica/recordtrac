244ff10d7dd742b920e3d37e61d188f8dc3bbad1
Joel Castillo <Joel.Castillo@nyu.edu>
2015-09-24 00:14:41 -0400

    Updated validation and new request form

--- public_records_portal/static/js/newrequestform.js
+++ public_records_portal/static/js/newrequestform.js
@@ -1,93 +1,122 @@
-$(function(){
-	$('#step1').fadeIn('fast');  
+$(function() {
+    $('#step1').fadeIn('fast');
 
-	var validator = new FormValidator('submitRequest', [{
-    	name: 'request_category',
-    	display: 'Request Category',
-    	rules: 'required'
-	},
-	{
-    	name: 'request_agency',
-    	display: 'Request Agency',
-    	rules: 'required'
-	},
-	{
-    	name: 'request_summary',
-    	display: 'Topic',
-    	rules: 'required'
-	},
-	{
-    	name: 'request_text',
-    	display: 'Request Details',
-    	rules: 'required'
-	},
-	{
-    	name: 'request_first_name',
-    	display: 'Requester First Name',
-    	rules: 'required'
-	},
-	{
-    	name: 'request_last_name',
-    	display: 'Request Last Name',
-    	rules: 'required'
-	}], function(errors, event) {
-    		if (errors.length > 0) {
-        	// Show the errors
-       		$('#step2').fadeOut('fast', function() {});
+    var validator = new FormValidator('submitRequest', [{
+        name: 'request_agency',
+        display: 'Request Agency',
+        rules: 'required'
+    }, {
+        name: 'request_summary',
+        display: 'Topic',
+        rules: 'required'
+    }, {
+        name: 'request_text',
+        display: 'Request Details',
+        rules: 'required'
+    }, {
+        name: 'request_first_name',
+        display: 'Requester First Name',
+        rules: 'required'
+    }, {
+        name: 'request_last_name',
+        display: 'Request Last Name',
+        rules: 'required'
+    }], function(errors, event) {
+        if (errors.length > 0) {
+            // Show the errors
+            $('#step2').fadeOut('fast', function() {});
 
-      		var errorString = '';
-		for (var i = 0, errorLength = errors.length; i < errorLength; i++) {
-                	errorString += errors[i].message + '<br />';
-                }
-                var stepId = '#step2';
-                var toolTipId = '#detailsTitle';
-                
-		if (errorString.indexOf("Request Category") >= 0 || errorString.indexOf("Request Agency") >= 0 || errorString.indexOf("Topic") >= 0)
-                {
-			stepId = '#step1';
-			toolTipId = '#categoryTitle';
-                }
+            var errorString = '';
+            for (var i = 0, errorLength = errors.length; i < errorLength; i++) {
+                errorString += errors[i].message + '<br />';
+            }
+            var stepId = '#step2';
+            var toolTipId = '#detailsTitle';
 
-      		$(stepId).fadeIn('slow', function() {
-			$(toolTipId).tooltip({content: errorString});
-			$(toolTipId).tooltip().mouseover();
-                        $('html, body').animate({
-                		scrollTop: $("#submitRequest").offset().top
-            		}, 500);
-      		});
+            if (errorString.indexOf("Request Category") >= 0 || errorString.indexOf("Request Agency") >= 0 || errorString.indexOf("Topic") >= 0) {
+                stepId = '#step1';
+                toolTipId = '#categoryTitle';
+            }
 
-    }
-});
-	  
-	$('#button1').click(function(){
-	    // scroll to the top of the form
-	    $('html, body').animate({
-	        scrollTop: $("#submitRequest").offset().top
-	    }, 500);
+            $(stepId).fadeIn('slow', function() {
+                $(toolTipId).tooltip({
+                    content: errorString
+                });
+                $(toolTipId).tooltip().mouseover();
+                $('html, body').animate({
+                    scrollTop: $("#submitRequest").offset().top
+                }, 500);
+            });
 
-	    // fade out step1, fade in step2
-		$('#step1').fadeOut('fast', function() {
-		  }); 
-		$('#step2').fadeIn('slow', function() {
-		  });  
-	});	
-	
-	$('#button2').click(function(){
-	 
-	 
-	});
+        }
+    });
 
-	$('#back').click(function(){
-		// scroll to the top of the form
-	    $('html, body').animate({
-	        scrollTop: $("#submitRequest").offset().top
-	    }, 500);
+    $('#button1').click(function() {
+        // scroll to the top of the form
+        $('html, body').animate({
+            scrollTop: $("#submitRequest").offset().top
+        }, 500);
 
-	    // fade out step1, fade in step2
-		$('#step2').fadeOut('fast', function() {
-		  }); 
-		$('#step1').fadeIn('slow', function() {
-		  });  
-	});	
+        // fade out step1, fade in step2
+        $('#step1').fadeOut('fast', function() {});
+        $('#step2').fadeIn('slow', function() {});
+    });
+
+    $('#button2').click(function() {
+
+
+    });
+
+    $('#back').click(function() {
+        // scroll to the top of the form
+        $('html, body').animate({
+            scrollTop: $("#submitRequest").offset().top
+        }, 500);
+
+        // fade out step1, fade in step2
+        $('#step2').fadeOut('fast', function() {});
+        $('#step1').fadeIn('slow', function() {});
+    });
+    $('#s1 a').click(function() {
+        // when step one is clicked, go to step 1
+
+        if (Modernizr.touch) {
+            $('html, body').animate({
+                scrollTop: $("#submitRequest").offset().top
+            }, 500);
+        } else {
+            // don't scroll to the top
+        }
+
+
+        $('#step2').fadeOut('fast', function() {});
+        $("#s2").removeClass("active");
+        $("#s1").addClass("active");
+        $('#step1').fadeIn('slow', function() {});
+    });
+
+    $('#s2 a').click(function() {
+        // when step two is clicked, go to step 1
+
+        // validation
+
+        if (!$('#agency').val() || !$('#request_summary').val()) {
+            alert("All indicated information is required. Please complete all fields");
+            exit();
+        }
+
+        if (Modernizr.touch) {
+            $('html, body').animate({
+                scrollTop: $("#submitRequest").offset().top
+            }, 500);
+        } else {
+            // don't scroll to the top
+        }
+
+        $('#step1').fadeOut('fast', function() {});
+        $("#s1").removeClass("active");
+        $("#s2").addClass("active");
+        $('#step2').fadeIn('slow', function() {});
+    });
 
 });
