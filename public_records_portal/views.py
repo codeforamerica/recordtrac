bde49df59e6df16e0263bb312797cd4d72025727
Swaad Golam <administrator@WebDev-iMac-004.local>
2015-10-02 11:28:48 -0400

    new file:   public_records_portal/templates/403.html 	new file:   public_records_portal/templates/500.html 	new file:   public_records_portal/templates/501.html 	modified:   public_records_portal/views.py

--- public_records_portal/views.py
+++ public_records_portal/views.py
@@ -1294,3 +1294,15 @@ def submit():
         pass
     else:
         pass
+@app.errorhandler(404):
+def page_not_found(e):
+    return render_template('404.html'), 404
+@app.errorhandler(500):
+def internal_server_error(e):
+    return render_template('500.html'), 500
+@app.errorhandler(403):
+def access_denied(e):
+    return render_template('403.html'), 403
+@app.errorhandler(501)
+def unexplained_error(e):
+    return render_template('501.html'), 501
