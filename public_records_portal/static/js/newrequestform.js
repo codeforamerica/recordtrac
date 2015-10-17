bae7b54164e730305234a6b59af2714c82e1a191
Brandon Tang <blt264@nyu.edu>
2015-10-16 21:05:15 -0400

    Changed request_summary to 90 max characters, working on javascript form validation

--- public_records_portal/static/js/newrequestform.js
+++ public_records_portal/static/js/newrequestform.js
@@ -177,4 +177,15 @@ $(function() {
         $('#step2').fadeIn('slow', function() {});
     });
 
+    function maxLength(el) {    
+    if (!('maxLength' in el)) {
+        var max = el.attributes.maxLength.value;
+        el.onkeypress = function () {
+            if (this.value.length >= max) return false;
+        };
+    }
+}
+
+maxLength(document.getElementById("request_summary"));
+
 });
