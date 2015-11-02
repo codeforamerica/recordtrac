081c4808b9dbef963510c3b32d4572a0422bed38
Swaad Golam <administrator@WebDev-iMac-004.local>
2015-11-02 18:31:56 -0500

    modified:   public_records_portal/views.py 	Socket ICAP implementation

--- public_records_portal/views.py
+++ public_records_portal/views.py
@@ -640,7 +640,7 @@ def fetch_requests(output_results_only=False, filters_map=None, date_format='%Y-
         requester_name = get_filter_value(filters_map, 'requester_name')
         page_number = int(get_filter_value(filters_map, 'page_number') or '1')
         request_id_search = get_filter_value(filters_map, 'request_id_search')
- 
+
     # Set initial checkboxes for mine_as_poc and mine_as_helper when redirected from login page
     if request.referrer and 'login' in request.referrer:
         if current_user.is_authenticated and (current_user.role in ['Portal Administrator', 'Agency Administrator'] or current_user.is_admin()):
@@ -1044,6 +1044,219 @@ def login():
 def get_attachments(resource):
     app.logger.info("\n\ngetting attachment file")
     return send_from_directory(app.config["UPLOAD_FOLDER"], resource, as_attachment=True)
+# app.route("/attachments/<string:resource>", meothfs=["GET"])
+# def check_icap(resource):
+#         #!/usr/bin/env python
+#     #
+#     # icap-client.py
+#     # Copyright (c) 2013 Uditha Atukorala
+#     #
+
+#     # simple icap client to test a server implementation
+
+#     import socket
+#     import sys
+
+#     HOST    = 'localhost'
+#     SERVICE = 'icap://icap.server.net/sample-service'
+#     PORT    = 1344
+
+#     # OPTIONS
+#     print "----- OPTIONS -----"
+#     try:
+#         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     except socket.error, msg:
+#         sys.stderr.write("[ERROR] %s\n" % msg[1])
+#         sys.exit(1)
+
+#     try:
+#         sock.connect((HOST, PORT))
+#     except socket.error, msg:
+#         sys.stderr.write("[ERROR] %s\n" % msg[1])
+#         sys.exit(2)
+
+#     sock.send( "OPTIONS %s ICAP/1.0\r\n" % ( SERVICE ) )
+#     sock.send( "Host: %s\r\n" % ( HOST ) )
+#     sock.send( "User-Agent: Python ICAP tester\r\n" )
+#     sock.send( "\r\n" )
+
+
+#     data = sock.recv(1024)
+#     string = ""
+#     while len(data):
+#         string = string + data
+#         data = sock.recv(1024)
+#     sock.close()
+
+#     print string
+
+
+#     # REQMOD, GET
+#     print "----- REQMOD - GET -----"
+#     try:
+#         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     except socket.error, msg:
+#         sys.stderr.write("[ERROR] %s\n" % msg[1])
+#         sys.exit(1)
+
+#     try:
+#         sock.connect((HOST, PORT))
+#     except socket.error, msg:
+#         sys.stderr.write("[ERROR] %s\n" % msg[1])
+#         sys.exit(2)
+
+#     sock.send( "REQMOD %s ICAP/1.0\r\n" % ( SERVICE ) )
+#     sock.send( "Host: %s\r\n" % ( HOST ) )
+#     sock.send( "Encapsulated: req-hdr=0, null-body=170\r\n" )
+#     sock.send( "\r\n" )
+
+#     sock.send( "GET / HTTP/1.1\r\n" )
+#     sock.send( "Host: www.origin-server.com\r\n" )
+#     sock.send( "Accept: text/html, text/plain\r\n" )
+#     sock.send( "Accept-Encoding: compress\r\n" )
+#     sock.send( "Cookie: ff39fk3jur@4ii0e02i\r\n" )
+#     sock.send( "If-None-Match: \"xyzzy\", \"r2d2xxxe\"\r\n" )
+#     sock.send( "\r\n" )
+
+
+#     data = sock.recv(1024)
+#     string = ""
+#     while len(data):
+#         string = string + data
+#         data = sock.recv(1024)
+#     sock.close()
+
+#     print string
+
+
+#     # REQMOD, POST
+#     print "----- REQMOD - POST -----"
+#     try:
+#         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     except socket.error, msg:
+#         sys.stderr.write("[ERROR] %s\n" % msg[1])
+#         sys.exit(1)
+
+#     try:
+#         sock.connect((HOST, PORT))
+#     except socket.error, msg:
+#         sys.stderr.write("[ERROR] %s\n" % msg[1])
+#         sys.exit(2)
+
+#     sock.send( "REQMOD %s ICAP/1.0\r\n" % ( SERVICE ) )
+#     sock.send( "Host: %s\r\n" % ( HOST ) )
+#     sock.send( "Encapsulated: req-hdr=0, req-body=147\r\n" )
+#     sock.send( "\r\n" )
+
+#     sock.send( "POST /origin-resource/form.pl HTTP/1.1\r\n" )
+#     sock.send( "Host: www.origin-server.com\r\n" )
+#     sock.send( "Accept: text/html, text/plain\r\n" )
+#     sock.send( "Accept-Encoding: compress\r\n" )
+#     sock.send( "Pragma: no-cache\r\n" )
+#     sock.send( "\r\n" )
+#     sock.send( "1e\r\n" )
+#     sock.send( "I am posting this information.\r\n" )
+#     sock.send( "0\r\n" )
+#     sock.send( "\r\n" )
+
+#     data = sock.recv(1024)
+#     string = ""
+#     while len(data):
+#         string = string + data
+#         data = sock.recv(1024)
+#     sock.close()
+
+#     print string
+
+
+#     # REQMOD - Message preview
+#     print "----- REQMOD - Message preview -----"
+#     try:
+#         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     except socket.error, msg:
+#         sys.stderr.write("[ERROR] %s\n" % msg[1])
+#         sys.exit(1)
+
+#     try:
+#         sock.connect((HOST, PORT))
+#     except socket.error, msg:
+#         sys.stderr.write("[ERROR] %s\n" % msg[1])
+#         sys.exit(2)
+
+#     sock.send( "REQMOD %s ICAP/1.0\r\n" % ( SERVICE ) )
+#     sock.send( "Host: %s\r\n" % ( HOST ) )
+#     sock.send( "Preview: 1024\r\n" )
+#     sock.send( "Encapsulated: req-hdr=0, req-body=147\r\n" )
+#     sock.send( "\r\n" )
+
+#     sock.send( "POST /origin-resource/form.pl HTTP/1.1\r\n" )
+#     sock.send( "Host: www.origin-server.com\r\n" )
+#     sock.send( "Accept: text/html, text/plain\r\n" )
+#     sock.send( "Accept-Encoding: compress\r\n" )
+#     sock.send( "Pragma: no-cache\r\n" )
+#     sock.send( "\r\n" )
+#     sock.send( "1e\r\n" )
+#     sock.send( "I am posting this information.\r\n" )
+#     sock.send( "0; ieof\r\n\r\n" )
+
+#     data = sock.recv(1024)
+#     string = ""
+#     while len(data):
+#         string = string + data
+#         data = sock.recv(1024)
+#     sock.close()
+
+#     print string
+
+
+#     # RESPMOD
+#     print "----- RESPMOD -----"
+#     try:
+#         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     except socket.error, msg:
+#         sys.stderr.write("[ERROR] %s\n" % msg[1])
+#         sys.exit(1)
+
+#     try:
+#         sock.connect((HOST, PORT))
+#     except socket.error, msg:
+#         sys.stderr.write("[ERROR] %s\n" % msg[1])
+#         sys.exit(2)
+
+#     sock.send( "RESPMOD %s ICAP/1.0\r\n" % ( SERVICE ) )
+#     sock.send( "Host: %s\r\n" % ( HOST ) )
+#     sock.send( "Encapsulated: req-hdr=0, res-hdr=137, res-body=296\r\n" )
+#     sock.send( "\r\n" )
+
+#     sock.send( "GET /origin-resource HTTP/1.1\r\n" )
+#     sock.send( "Host: www.origin-server.com\r\n" )
+#     sock.send( "Accept: text/html, text/plain, image/gif\r\n" )
+#     sock.send( "Accept-Encoding: gzip, compress\r\n" )
+#     sock.send( "\r\n" )
+#     sock.send( "HTTP/1.1 200 OK\r\n" )
+#     sock.send( "Date: Mon, 10 Jan 2000 09:52:22 GMT\r\n" )
+#     sock.send( "Server: Apache/1.3.6 (Unix)\r\n" )
+#     sock.send( 'ETag: "63840-1ab7-378d415b"\r\n' )
+#     sock.send( "Content-Type: text/html\r\n" )
+#     sock.send( "Content-Length: 51\r\n" )
+#     sock.send( "\r\n" )
+#     sock.send( "33\r\n" )
+#     sock.send( "This is data that was returned by an origin server.\r\n" )
+#     sock.send( "0\r\n" )
+#     sock.send( "\r\n" )
+
+#     data = sock.recv(1024)
+#     string = ""
+#     while len(data):
+#         string = string + data
+#         data = sock.recv(1024)
+#     sock.close()
+
+#     print string
+
+
+#     sys.exit(0)
+
 
 
 @app.route("/pdfs/<string:resource>", methods=["GET"])
