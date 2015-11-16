adda9436b3f81374def11d529a448323e3e27f56
Joel Castillo <Joel.Castillo@nyu.edu>
2015-11-15 23:57:44 -0500

    Removed unnecessary code; Added docstring

--- public_records_portal/upload_helpers.py
+++ public_records_portal/upload_helpers.py
@@ -7,8 +7,10 @@
 """
 
 import os
-from werkzeug import secure_filename
-import pyclamd
+import socket
+
+from werkzeug.utils import secure_filename
+
 from public_records_portal import app
 
 
@@ -19,12 +21,15 @@ def should_upload():
         return True
     return False
 
+
 # These are the extensions that can be uploaded:
-ALLOWED_EXTENSIONS = ['txt', 'pdf', 'doc', 'rtf','odt', 'odp', 'ods', 'odg', 'odf',
-                     'ppt', 'pps', 'xls', 'docx', 'pptx', 'ppsx', 'xlsx']
-HOST=app.config['HOST']
-SERVICE=app.config['SERVICE']
-PORT=app.config['PORT']
+ALLOWED_EXTENSIONS = ['txt', 'pdf', 'doc', 'rtf', 'odt', 'odp', 'ods', 'odg',
+                      'odf',
+                      'ppt', 'pps', 'xls', 'docx', 'pptx', 'ppsx', 'xlsx']
+HOST = app.config['HOST']
+SERVICE = app.config['SERVICE']
+PORT = app.config['PORT']
+
 
 def get_download_url(doc_id, record_id=None):
     if not should_upload():
@@ -33,7 +38,16 @@ def get_download_url(doc_id, record_id=None):
 
 # @timeout(seconds=20)
 def upload_file(document, request_id):
-    # Uploads file to local directory, returns upload_path, filename
+    """
+    Takes an uploaded file, scans it using an ICAP Scanner, and stores the
+    file if the scan passed
+    :param document: File
+    :type document:
+    :param request_id: Current request
+    :type request_id:
+    :return:
+    :rtype:
+    """
     app.logger.info("\n\nLocal upload file")
     if not should_upload():
         app.logger.info("\n\nshoud not upload file")
@@ -53,38 +67,40 @@ def upload_file(document, request_id):
             print("[ERROR] %s\n" % msg[1])
             print("Unable to verify file for malware. Please try again.")
 
-        sock.send( "REQMOD %s ICAP/1.0\r\n" % ( SERVICE ) )
-        sock.send( "Host: %s\r\n" % ( HOST ) )
-        sock.send( "Encapsulated: req-hdr=0, null-body=170\r\n" )
-        sock.send( "\r\n" )
+        sock.send("REQMOD %s ICAP/1.0\r\n" % (SERVICE))
+        sock.send("Host: %s\r\n" % (HOST))
+        sock.send("Encapsulated: req-hdr=0, null-body=170\r\n")
+        sock.send("\r\n")
 
-        sock.send( "POST / HTTP/1.1\r\n" )
-        sock.send( "Host: www.origin-server.com\r\n" )
-        sock.send( "Accept: text/html, text/plain\r\n" )
-        sock.send( "Accept-Encoding: compress\r\n" )
-        sock.send( "Cookie: ff39fk3jur@4ii0e02i\r\n" )
-        sock.send( "If-None-Match: \"xyzzy\", \"r2d2xxxe\"\r\n" )
-        sock.send( "\r\n" )
+        sock.send("POST / HTTP/1.1\r\n")
+        sock.send("Host: www.origin-server.com\r\n")
+        sock.send("Accept: text/html, text/plain\r\n")
+        sock.send("Accept-Encoding: compress\r\n")
+        sock.send("Cookie: ff39fk3jur@4ii0e02i\r\n")
+        sock.send("If-None-Match: \"xyzzy\", \"r2d2xxxe\"\r\n")
+        sock.send("\r\n")
         with open(document, "rb") as uploadedFile:
-          f = uploadedFile.read()
-          b = bytearray(f)
-        sock.send( uploadedFile )
-        sock.send( "OPTIONS icap://"+app.config(['HOST'])+":"+app.config(['PORT'])+"/wwrespmod?profile=default ICAP/1.0" )
-        sock.send( "Host: %s\r\n" % ( HOST ) )
-        sock.send( "ICAP/1.0 200 OK" )
-        sock.send( "Methods: REQMOD, RESPMOD")
-        sock.send( "Options-TTL: 3600")
-        sock.send( "Encapsulated: null-body=0")
-        sock.send( "Max-Connections: 400")
-        sock.send( "Preview: 30")
-        sock.send( "Service: McAfee Web Gateway 7.5.2 build 20202")
-        sock.send( '''ISTag: "00004154-2.26.156-00007980"''')
-        sock.send( "Allow: 204")
+            f = uploadedFile.read()
+            b = bytearray(f)
+        sock.send(uploadedFile)
+        sock.send("OPTIONS icap://" + app.config(['HOST']) + ":" + app.config(
+            ['PORT']) + "/wwrespmod?profile=default ICAP/1.0")
+        sock.send("Host: %s\r\n" % (HOST))
+        sock.send("ICAP/1.0 200 OK")
+        sock.send("Methods: REQMOD, RESPMOD")
+        sock.send("Options-TTL: 3600")
+        sock.send("Encapsulated: null-body=0")
+        sock.send("Max-Connections: 400")
+        sock.send("Preview: 30")
+        sock.send("Service: McAfee Web Gateway 7.5.2 build 20202")
+        sock.send('''ISTag: "00004154-2.26.156-00007980"''')
+        sock.send("Allow: 204")
 
         data = sock.recv(2048)
         string = data
         if "200 OK" in string:
-            app.logger.info("\n\n%s is allowed: %s" % (document.filename, allowed[0]))
+            app.logger.info(
+                "\n\n%s is allowed: %s" % (document.filename, string))
             filename = secure_filename(document.filename)
             upload_path = upload_file_locally(document, filename, request_id)
             return upload_path, filename
@@ -103,11 +119,6 @@ def upload_file_locally(document, filename, request_id):
     app.logger.info("\n\nupload path: %s" % (upload_path))
 
     document.save(upload_path)
-
-    # if clamdiagnostic.scan_file(upload_path) is not None:
-    #     os.remove(upload_path)
-    #     app.logger.info("\n\nVirus found in uploaded file, file deleted")
-    #     return "VIRUS_FOUND"
 
     app.logger.info("\n\nfile uploaded to local successfully")
 
