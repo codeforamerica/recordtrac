dbb1964e386c3ffa286e460e0d738e14e3eb0935
Joel Castillo <jcastillo@records.nyc.gov>
2015-11-19 08:20:57 -0500

    Updated user list for performance testing

--- users/pt_user_setup.py
+++ users/pt_user_setup.py
@@ -1,10 +1,9 @@
-agencies = [
+non_partner_agencies = [
     "Administration for Children's Services",
     "Art Commission",
     "Board of Correction",
     "Board of Standards and Appeals",
     "Business Integrity Commission",
-    "City Commission on Human Rights",
     "Civil Service Commission",
     "Civilian Complaint Review Board",
     "Commission to Combat Police Corruption",
@@ -17,17 +16,14 @@ agencies = [
     "Department of Correction",
     "Department of Cultural Affairs",
     "Department of Design and Construction",
-    "Department of Education",
     "Department of Environmental Protection",
     "Department of Finance",
     "Department of Health and Mental Hygiene",
     "Department of Homeless Services",
     "Department of Housing Preservations and Development",
-    "Department of Information Technology and Telecommunications",
     "Department of Investigation",
     "Department of Parks and Recreation",
     "Department of Probation",
-    "Department of Records and Information Services",
     "Department of Transporation",
     "Department of Youth and Community Development",
     "Design Commission",
@@ -38,20 +34,14 @@ agencies = [
     "Landmarks Preservation Commission",
     "Law Department",
     "Loft Board",
-    "Mayor's Office of Contract Services",
-    "Mayor's Office of Media and Entertainment",
     "New York City Fire Department",
     "New York City Housing Authority",
-    "Office of Administrative Trials and Hearings",
-    "Office of the Chief Medical Examiner",
-    "Office of Emergency Management",
     "Office of Environmental Remediation",
     "Office of Labor Relations",
     "Office of Long-Term Planning & Sustainability",
     "Office of Management and Budget",
     "Office of Payroll Administration",
     "Office of the Actuary",
-    "Office of the Mayor",
     "Office of the Special Narcotics Prosecutor",
     "Police Department",
     "Procurement Policy Board",
@@ -59,15 +49,26 @@ agencies = [
     "Small Business Services",
     "Taxi and Limousine Commission"
 ]
-
+partner_agencies = [
+    "City Commission on Human Rights",
+    "Department of Education",
+    "Department of Information Technology and Telecommunications",
+    "Department of Records and Information Services",
+    "Mayor's Office of Contract Services",
+    "Mayor's Office of Media and Entertainment",
+    "Office of Administrative Trials and Hearings",
+    "Office of the Chief Medical Examiner",
+    "Office of Emergency Management",
+    "Office of the Mayor",
+]
 
 k = 101
 staff = open("staff_pt.csv", "w")
 liaisons = open("liaisons_pt.csv", "w")
 
-for i in range(len(agencies)-1):
-    liaisons.write("%s,cpuiptuser000%s@mailinator.com\n" % (agencies[i], k))
-    for j in range(6):
+for i in range(len(partner_agencies) - 1):
+    liaisons.write("%s,cpuiptuser000%s@mailinator.com\n" % (partner_agencies[i], k))
+    for j in range(30):
         staff.write("PTFname PTLname,cpuiptuser000%s@mailinator.com,%s,311\n" %
-                    (k, agencies[i]))
-        k = k + 1
+                    (k, partner_agencies[i]))
+        k += 1
