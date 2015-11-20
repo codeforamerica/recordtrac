6f1dda83aaec35db3dd4bfd33290d18120add9fa
Joel Castillo <jcastillo@records.nyc.gov>
2015-11-20 03:53:43 -0500

    Added dynamic linking to NYC.gov assets

--- public_records_portal/__init__.py
+++ public_records_portal/__init__.py
@@ -57,6 +57,7 @@ envvars = [
     'DAYS_AFTER_EXTENSION',  # Default number of days for an extension
     'DAYS_TO_FULFILL',  # Default number of days to fulfill a request
     'DAYS_UNTIL_OVERDUE',  # Default number of days until
+    'NYC_GOV_BASE', # Base URL for NYC.gov links/assets
 
     # Flask Settings
     'APPLICATION_URL',  # Application URL
