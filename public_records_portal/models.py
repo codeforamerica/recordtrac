0acb9c23ef3b8b3f1912b29d87cc43d9b78d1f6e
Joel Castillo <jcastillo@records.nyc.gov>
2015-11-25 13:15:10 -0500

    Removed unique requirement on request_text

--- public_records_portal/models.py
+++ public_records_portal/models.py
@@ -287,7 +287,7 @@ class Request(db.Model):
                        order_by='QA.date_created.desc()')  # The list of QA units for this request
     status_updated = db.Column(db.DateTime)
     summary = db.Column(db.String(5000), nullable=False)
-    text = db.Column(db.String(5000), unique=True,
+    text = db.Column(db.String(5000),
                      nullable=False)  # The actual request text.
     owners = relationship('Owner', cascade='all, delete',
                           order_by='Owner.date_created.asc()')
