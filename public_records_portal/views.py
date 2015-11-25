1b7eed9f1739458202d23e4f0338b216480269de
Joel Castillo <jcastillo@records.nyc.gov>
2015-11-24 19:05:01 -0500

    Revert "Adding NYC_GOV_BASE .env"
    This reverts commit 9fd11f1b98f8d5ac415c42abb5f0851f9b207f78.

--- public_records_portal/views.py
+++ public_records_portal/views.py
@@ -436,12 +436,9 @@ def explain_all_actions():
 
 @app.route("/<string:audience>/request/<string:request_id>")
 def show_request_for_x(audience, request_id):
-    proper_request_id = re.match("FOIL-\d{4}\d{3}-\d{5}", request_id)
-    if proper_request_id:
-        if "city" in audience:
-            return show_request_for_city(request_id=request_id)
-        return show_request(request_id=request_id, template="manage_request_%s.html" % (audience))
-    return bad_request(400)
+    if "city" in audience:
+        return show_request_for_city(request_id=request_id)
+    return show_request(request_id=request_id, template="manage_request_%s.html" % (audience))
 
 
 show_request_for_x.methods = ['GET', 'POST']
