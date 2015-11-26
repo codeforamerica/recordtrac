7f2f26ffe1224e6d3b464aa4131b9a38db387e19
swaadg <swaad.golam@gmail.com>
2015-11-26 03:56:16 -0500

    Added 401 error handling

--- public_records_portal/views.py
+++ public_records_portal/views.py
@@ -1516,6 +1516,13 @@ def any_page(page):
 def bad_request(e):
     return render_template("400.html"), 400
 
+@app.errorhandler(401)
+def unauthorized(e):
+    return render_template("401.html"), 401
+
+@app.errorhandler(403)
+def access_denied(e):
+    return redirect(url_for('login'))
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -1525,11 +1532,6 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
-
-
-@app.errorhandler(403)
-def access_denied(e):
-    return redirect(url_for('login'))
 
 
 @app.errorhandler(501)
