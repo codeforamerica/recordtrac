4cbfb0dd0e7b4550ee6f62e004a8c037a56d7b41
swaadg <swaad.golam@gmail.com>
2015-11-26 04:08:31 -0500

    Added 405 bad_method handling

--- public_records_portal/views.py
+++ public_records_portal/views.py
@@ -1528,6 +1528,10 @@ def access_denied(e):
 def page_not_found(e):
     return render_template("404.html"), 404
 
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return render_template("405.html"), 405
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
