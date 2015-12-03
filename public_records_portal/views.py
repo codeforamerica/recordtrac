6914e09aedbc210ed00f6216c38a84b9fefa5584
Joel Castillo <jcastillo@records.nyc.gov>
2015-12-03 15:09:02 -0500

    Applied CSRF to login page

--- public_records_portal/views.py
+++ public_records_portal/views.py
@@ -60,11 +60,6 @@ zip_reg_ex = re.compile('^[0-9]{5}(?:-[0-9]{4})?$')
 def make_session_permanent():
     app.permanent_session_lifetime = timedelta(minutes=180)
 
-def generate_csrf_token():
-    if '_csrf_token' not in session:
-        session['_csrf_token'] = str(uuid4())
-    return session['_csrf_token']
-app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 @app.before_request
 def csrf_protect():
@@ -73,6 +68,12 @@ def csrf_protect():
         if not token or token != request.form.get('_csrf_token'):
             return access_denied(403)
 
+def generate_csrf_token():
+    if '_csrf_token' not in session:
+        session['_csrf_token'] = str(uuid4())
+        print session['_csrf_token']
+    return session['_csrf_token']
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 
 # Submitting a new request
