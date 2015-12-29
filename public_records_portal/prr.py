c8d2d158e7a5a5a57100643721d8cfcd590fbb98
August Tan <aot221@nyu.edu>
2015-12-29 01:25:29 -0500

    agency adding a record and extending a request email templates

--- public_records_portal/prr.py
+++ public_records_portal/prr.py
@@ -526,11 +526,11 @@ def add_offline_record(
                               access=access, description=description,
                               privacy=privacy)  # To create an offline record, we need to know the request ID to which it will be added, the user ID for the person adding the record, how it can be accessed, and a description/title of the record.
     if record_id:
+        notification_content['department_name'] = department_name
         change_request_status(request_id, 'A response has been added.')
         generate_prr_emails(request_id=request_id,
-                            notification_type='City response added',
-                            text=description,
-                            department_name=department_name)
+                            notification_type='city_response_added',
+                            notification_content=notification_content)
         add_staff_participant(request_id=request_id, user_id=user_id)
         return record_id
     return False
@@ -557,7 +557,7 @@ def add_link(
         notification_content['description'] = description
         notification_content['department_name'] = department_name
         generate_prr_emails(request_id=request_id,
-                            notification_type='City response added',
+                            notification_type='city_response_added',
                             notification_content=notification_content)
         add_staff_participant(request_id=request_id, user_id=user_id)
         return record_id
