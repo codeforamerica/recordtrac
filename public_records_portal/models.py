cbc9ffffadf81815f39a31004a374ab96477df76
estevez90 <hestevez@records.nyc.gov>
2016-01-21 14:52:59 -0500

    Show Department Filter for Agency FOIL Officer

--- public_records_portal/models.py
+++ public_records_portal/models.py
@@ -142,7 +142,7 @@ class User(db.Model):
         return 'N/A'
 
     def show_department_filters(self):
-        return self.role in ['Portal Administrator', 'Agency Administrator', 'Agency FOIL Personnel']
+        return self.role in ['Portal Administrator', 'Agency Administrator', 'Agency FOIL Personnel', 'Agency FOIL Officer']
 
     def __init__(
             self,
