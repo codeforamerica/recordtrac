f4ac0e41c48ba977bf592ff8130302406565ea83
Joel Castillo <jcastillo@records.nyc.gov>
2016-01-26 10:59:30 -0500

    Re-Applied OP-470 fix

--- public_records_portal/models.py
+++ public_records_portal/models.py
@@ -142,7 +142,7 @@ class User(db.Model):
         return 'N/A'
 
     def show_department_filters(self):
-        return self.role in ['Portal Administrator', 'Agency Administrator', 'Agency FOIL Officer']
+        return self.role in ['Portal Administrator', 'Agency Administrator']
 
     def __init__(
             self,
