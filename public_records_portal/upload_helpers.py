e8e0e7abe28a3c0280dbdad954e6e1d4a8e5dbc1
Joel Castillo <jcastillo@records.nyc.gov>
2016-02-09 09:25:05 -0500

    Refactoring upload code for ICAP

--- public_records_portal/upload_helpers.py
+++ public_records_portal/upload_helpers.py
@@ -27,8 +27,7 @@ def should_upload():
 
 
 # These are the extensions that can be uploaded:
-ALLOWED_EXTENSIONS = ['txt', 'pdf', 'doc', 'rtf', 'odt', 'odp', 'ods', 'odg',
-                      'odf',
+ALLOWED_EXTENSIONS = ['txt', 'pdf', 'doc', 'rtf', 'odt', 'odp', 'ods', 'odg', 'odf',
                       'ppt', 'pps', 'xls', 'docx', 'pptx', 'ppsx', 'xlsx',
                       'jpg','jpeg','png','gif','tif','tiff','bmp',
                       'avi','flv','wmv','mov','mp4',
@@ -53,99 +52,50 @@ def upload_file(document, request_id, privacy = True):
     Takes an uploaded file, scans it using an ICAP Scanner, and stores the
     file if the scan passed
     :param document: File
-    :type document:
-    :param request_id: Current request
-    :type request_id:
+    :param request_id: Current request identifier
     :return:
-    :rtype:
     """
-
-    app.logger.info("\n\nLocal upload file")
     if not should_upload():
-        app.logger.info("\n\nshoud not upload file")
-        return '1', None, None  # Don't need to do real uploads locally
-    if app.config["SHOULD_SCAN_FILES"] == 'True':
-        if allowed_file(document.filename) and len(document.read()) > 15000000:
-            app.logger.error("Error with filesize.")
-            error = "file_too_large"
-            return None, None, error
+        app.logger.info("Upload functionality has been disabled")
+        return '1', None, None
+
+    if app.config['SHOULD_SCAN_FILES'] == 'True':
+        file_length = document.read()
+        document.seek(0)
+        if file_length < 0:
+            app.logger.error("File: %s is too small" % document.filename)
+            return None, None, None
+        if file_length > app.config['MAX_FILE_SIZE']:
+            app.logger.error("File: %s is too large" % document.filename)
+            return None, None, None
         if allowed_file(document.filename):
-            app.logger.info("\n\nbegin file upload")
-            try:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            except socket.error, msg:
-                app.logger.error("Unable to bind socket and create connection to ICAP server.")
+            file_scanned = scan_file(document)
 
-            try:
-                sock.connect((SERVICE, PORT))
-            except socket.error, msg:
-                app.logger.error("[ERROR] %s\n" % msg[1])
-                app.logger.error("Unable to verify file for malware. Please try again.")
-            app.logger.info("----- RESPMOD -----")
-            try:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            except socket.error, msg:
-                sys.stderr.write("[ERROR] %s\n" % msg[1])
-                app.logger.error("Error at socket.")
-            try:
-                sock.connect((SERVICE, PORT))
-            except socket.error, msg:
-                sys.stderr.write("[ERROR] %s\n" % msg[1])
-                app.logger.error("Error at socket.")
-            today = datetime.date.today()
-            cDate = today.strftime("%a, %d %b %Y")
-            time = datetime.datetime.now()
-            cTime = time.strftime("%H:%M:%S")
-            sock.send( "RESPMOD %s ICAP/1.0\r\n" % ( SERVICE ) )
-            sock.send( "Host: %s\r\n" % ( HOST ) )
-            sock.send( "Encapsulated: req-hdr=0, res-hdr=137, res-body=296\r\n" )
-            sock.send( "\r\n" )
-            sock.send( "GET /origin-resource HTTP/1.1\r\n" )
-            sock.send( "Host: www.origin-server.com\r\n" )
-            mimetype = document.mimetype
-            sock.send( "Accept: " + mimetype + "\r\n")
-            sock.send( "Accept-Encoding: gzip, compress\r\n" )
-            sock.send( "\r\n" )
-            sock.send( "HTTP/1.1 200 OK\r\n" )
-            sock.send( "Date: "+cDate +" "+cTime+" GMT\r\n" )
-            sock.send( "Server: Apache/1.3.6 (Unix)\r\n" )
-            sock.send( 'ETag: "63840-1ab7-378d415b"\r\n' )
-            sock.send( "Content-Type: text/html\r\n" )
-            sock.send( "Content-Length: "+ str(len(document.read()))+"\r\n" )
-            document.seek(0)
-            sock.send( "\r\n" )
-            sock.send( "33\r\n" )
-            sock.send(document.read()+"\r\n")
-            sock.send( "0\r\n" )
-            sock.send( "\r\n" )
-            document.seek(0)
-            try:
-                data = sock.recv(1024)
-                string = data
-                app.logger.info(data)
-            except:
-                app.logger.error(traceback.format_exc())
-            if "200 OK" in string:
-                app.logger.info("\n\n%s is allowed: %s" % (document.filename, string))
-                filename = secure_filename(document.filename)
-                app.logger.info("rsyncing...")
-                app.logger.info(filename)
-                upload_path = upload_file_locally(document, filename, request_id, privacy)
-                if privacy == 'False':
-                    app.logger.info("rsync public...")
-                    subprocess.call(["rsync", "-avzh", "ssh", app.config['UPLOAD_PUBLIC_LOCAL_FOLDER'] + "/" + document.filename, app.config['PUBLIC_SERVER_USER'] + '@' + app.config['PUBLIC_SERVER_HOSTNAME'] + ':' + app.config['UPLOAD_PUBLIC_REMOTE_FOLDER'] + "/"])
-                elif privacy == 'True':
-                    app.logger.info("rsync private...")
-                return upload_path, filename, None
-            else:
-                app.logger.error("Malware detected. Upload failed")
-                sock.close()
-                return None, None, None
-        return None, None, None
-    return "1", secure_filename(document.filename), None
+def scan_file(document):
+    socket = connect_to_icap_server()
+    if not socket:
+        return None
+
+    icap_request = "RESPMOD icap://%s:%s/%s%s ICAP/1.0\r\n" % (app.config['ICAP_SERVER'], app.config['ICAP_PORT'], app.config['ICAP_SERVICE_NAME'], app.config['ICAP_PROFILE']
+    icap_request += "Allow: 204\r\n"
+    icap_request += "Encapsulated: req-hdr=%s res-hdr=%s res-body=%s\r\n" % ()
 
 
-def upload_file_locally(document, filename, request_id, privacy):
+def connect_to_icap_server():
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except socket.error, msg:
+        app.logger.error("Error binding socket\n%s\n" % msg[1])
+        return None
+
+    try:
+        sock.connect((app.config['ICAP_SERVER'], app.config['ICAP_PORT']))
+    except socket.error, msg:
+        app.logger.error("Error connecting to %s:%s\n%s" % (app.config['ICAP_SERVER'], app.config['ICAP_PORT'], msg[1]))
+        return None
+    return sock
+
+def upload_file_locally(document, filename, privacy):
     app.logger.info("\n\nuploading file locally")
     app.logger.info("\n\n%s" % (document))
 
